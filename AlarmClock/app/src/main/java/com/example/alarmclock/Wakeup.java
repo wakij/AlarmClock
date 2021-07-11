ef79901f22672d0eaab7d0dc89b952bf4a33d9b5
脇田知成 <tomoshigesansai1600@gmail.com>
2021-07-11 20:58:27 +0900

    追加

--- AlarmClock/app/src/main/java/com/example/alarmclock/Wakeup.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/Wakeup.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +15,8 @@ public class Wakeup extends AppCompatActivity {
 
     private TimePicker timePicker;
     private Button setbtn;
-    private EditText editAlarmName;
+    private Button backbtn;
+//    private EditText editAlarmName;
     int hour;
     int minutes;
 
@@ -25,7 +27,8 @@ public class Wakeup extends AppCompatActivity {
 
         timePicker = findViewById(R.id.time_picker);
         setbtn = findViewById(R.id.setbtn);
-        editAlarmName = findViewById(R.id.alarm_name);
+//        editAlarmName = findViewById(R.id.alarm_name);
+        backbtn = findViewById(R.id.backbtn);
 
         setbtn.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
@@ -33,10 +36,18 @@ public class Wakeup extends AppCompatActivity {
             public void onClick(View v) {
                 hour = timePicker.getHour();
                 minutes = timePicker.getMinute();
-                String alarmName = editAlarmName.getText().toString();
-                if (alarmName.equals("")){
-                    alarmName = "無題";
-                }
+//                String alarmName = editAlarmName.getText().toString();
+
+//                if (alarmName.equals("")){
+//                    alarmName = "無題";
+//                }
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
