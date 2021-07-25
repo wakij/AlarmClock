6810dc86462e112b5d9852c32cfb5add7cf0e454
脇田知成 <tomoshigesansai1600@gmail.com>
2021-07-25 22:37:38 +0900

    コメント追加しました

--- AlarmClock/app/src/main/java/com/example/alarmclock/AlarmBroadcastReceiver.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/AlarmBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+//これはアラーム信号を受け取るためのスクリプトです
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     @Override
@@ -12,7 +13,8 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         Toast.makeText(context,"Received", Toast.LENGTH_LONG).show();
 
         Intent startActivityIntent = new Intent(context, Wakeup.class);
-        startActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(startActivityIntent);
     }
 }
+
