4196082221ab3e21494fd39ee8d9ed35f8fa8e9f
hayabusa1gou <s3029524@outlook.jp>
2021-08-01 09:42:13 +0900

    タイトル

--- AlarmClock/app/src/main/java/com/example/alarmclock/title.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/title.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,22 +21,19 @@ public class title extends AppCompatActivity {
         decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         String DB_FULL_PATH = "data/data/" + getPackageName() + "/myDatabase1.db";
-        Button titlebtn =  findViewById(R.id.titlebtn);
+        ImageButton title =  findViewById(R.id.imageButton2);
 
-        titlebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    SQLiteDatabase checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH,null,SQLiteDatabase.OPEN_READONLY);
-                    checkDB.close();
-                    Intent intent= new Intent(getApplication(),MainActivity.class);
-                    startActivity(intent);
-                } catch (Exception e){
-                    Intent intent = new Intent(getApplication(),Explaination.class);
-                    startActivity(intent);
-                }
-
+        title.setOnClickListener(v -> {
+            try {
+                SQLiteDatabase checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH,null,SQLiteDatabase.OPEN_READONLY);
+                checkDB.close();
+                Intent intent= new Intent(getApplication(),MainActivity.class);
+                startActivity(intent);
+            } catch (Exception e){
+                Intent intent = new Intent(getApplication(),Explaination.class);
+                startActivity(intent);
             }
+
         });
     }
 }
